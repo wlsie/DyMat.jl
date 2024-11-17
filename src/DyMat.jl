f53910fc8580c5faef2d6b826d26090120d0e6d7
wlsie <dhumane.rohit.us@gmail.com>
2024-11-17 17:35:23 -0500

    added remaining functions

--- src/DyMat.jl
+++ src/DyMat.jl
@@ -1,6 +1,9 @@
 module DyMat
 
 export strMatNormal, strMatTrans, copysign
+export DyMatFile, blocks, getNames,data,block
+export description, sharedData, size, abscissa, sortByBlocks, nameTree, getVarArray
+
 include("functions.jl")
 
 end

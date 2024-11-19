10c624dc0c311b204df5937a39712520f487553e
wlsie <dhumane.rohit.us@gmail.com>
2024-11-19 08:45:28 -0500

    added external package dependencies

--- src/DyMat.jl
+++ src/DyMat.jl
@@ -1,5 +1,8 @@
 module DyMat
 
+using MAT
+using LinearAlgebra
+
 export strMatNormal, strMatTrans, copysign
 export DyMatFile, blocks, getNames,data,block
 export description, sharedData, size, abscissa, sortByBlocks, nameTree, getVarArray

55d4d88d8d73111b9eaee07cac674f0a2cee3f7d
wlsie <dhumane.rohit.us@gmail.com>
2024-11-23 09:18:28 -0500

    added test for loading mat files

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,8 @@ using Test
     @test strMatNormal(["ab   " "bc "]) == ["ab" "bc"]
     @test strMatTrans(["ab  " "bc"; "cd  " "de"]) == ["acbd"; "bdce"]
     @test sign(-6.0) == -1.0
+    @test length(getNames(DyMatFile("DoublePendulum_Dymola-7.4.mat")))==1095
+    @test length(getNames(DyMatFile("DoublePendulum_Dymola-2012-SaveAsPlotted.mat"))) == 5
+    @test length(getNames(DyMatFile("DoublePendulum_Dymola-2012.mat"))) == 1096
+    @test length(getNames(DyMatFile("DoublePendulum_OpenModelica-1.8.mat"))) == 2213
 end

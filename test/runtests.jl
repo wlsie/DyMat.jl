d713ff4d9919a7461df9248a9f0031f13b6b7cc9
wlsie <dhumane.rohit.us@gmail.com>
2024-11-23 09:03:22 -0500

    set basic tests for functions not involving mat file

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,7 @@ using DyMat
 using Test
 
 @testset "DyMat.jl" begin
-    # Write your tests here.
+    @test strMatNormal(["ab   " "bc "]) == ["ab" "bc"]
+    @test strMatTrans(["ab  " "bc"; "cd  " "de"]) == ["acbd"; "bdce"]
+    @test sign(-6.0) == -1.0
 end
